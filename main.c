66579f4eced284124a0c14fed563094c1d58bd27
marco-a <marco.agnoli@me.com>
2013-07-28 19:50:21 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,36 @@
+#include "utilities.h"
+
+int main(int argc, const char * argv[]) {
+  uint_t *array;
+	
+	// Allocate 10 ints
+	array = mem_alloc(10, uint_t);
+
+	if (array != nil) {
+		fprintf(stderr, "Sucessfully allocated %zu ints\n", mem_get_num_elements(array));
+
+		array[0] = 1;
+		array[1] = 3;
+		array[2] = 3;
+		array[3] = 7;
+
+		// Shrink array to 5 ints
+		if (mem_shrink(array, 5)) {
+			fprintf(stderr, "Successfully shrank to %zu ints\n", mem_get_num_elements(array));
+
+			// Grew array to 10 ints
+			if (mem_grow(array, 5)) {
+				fprintf(stderr, "Successfully grew to %zu ints\n", mem_get_num_elements(array));
+
+				fprintf(stderr, "Number is: %u%u%u%u\n", array[0], array[1], array[2], array[3]);
+
+				mem_dump(array);
+			}
+		}
+
+		if (mem_release(array)) {
+			fprintf(stderr, "Memory deallocated successfully!");
+		}
+	}
+}
+
